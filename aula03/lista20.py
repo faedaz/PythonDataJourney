d800155679b7855d2b5f80072db218566b64a454
Matheus linux <matheus.fnds20@gmail.com>
2025-06-03 08:47:30 -0300

    dicionario

---
+++ aula03/lista20.py
@@ -0,0 +1,19 @@
+#Biblioteca de livros
+
+biblioteca = [
+    {
+        "livro": "Senhor dos Aneis",
+        "autor": "Tolkien",
+        "publicaco": "1954"
+    },
+    {
+        "livro": "World of Warcraft",
+        "autor": "Blizzard Activison",
+        "publicacao": "2014"        
+    },
+    {
+        "livro": "Pequeno Principe",
+        "autor": "Matheus",
+        "ano": "1994"
+    }
+]
